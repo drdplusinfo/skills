1c15cf5c4532b3d3b2b2d027bccdbe99419d6424
jaroslavtyc <mail@jaroslavtyc.com>
2016-04-16 10:25:56 +0200

    Enum registrar updated according to library changes

--- DrdPlus/Person/Skills/EnumTypes/PersonSkillsEnumsRegistrar.php
+++ DrdPlus/Person/Skills/EnumTypes/PersonSkillsEnumsRegistrar.php
@@ -3,14 +3,14 @@ namespace DrdPlus\Person\Skills\EnumTypes;
 
 use Doctrineum\DateTimeImmutable\DateTimeImmutableType;
 use DrdPlus\Person\Background\EnumTypes\PersonBackgroundEnumsRegistrar;
-use DrdPlus\Person\ProfessionLevels\EnumTypes\ProfessionLevelsEnumsRegistrar;
+use DrdPlus\Person\ProfessionLevels\EnumTypes\ProfessionLevelsEnumRegistrar;
 
 class PersonSkillsEnumsRegistrar
 {
     public static function registerAll()
     {
         PersonBackgroundEnumsRegistrar::registerAll();
-        ProfessionLevelsEnumsRegistrar::registerAll();
+        ProfessionLevelsEnumRegistrar::registerAll();
         DateTimeImmutableType::registerSelf();
     }
 }
