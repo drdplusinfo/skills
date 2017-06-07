04d285582fb7e723c0ea02829e61f3ecb4c1f42b
Jaroslav Týc <mail@jaroslavtyc.com>
2017-06-07 13:23:16 +0200

    Cooking can create catch processing quality

--- DrdPlus/Skills/Combined/Cooking.php
+++ DrdPlus/Skills/Combined/Cooking.php
@@ -1,8 +1,11 @@
 <?php
 namespace DrdPlus\Skills\Combined;
 
+use Drd\DiceRolls\Templates\Rolls\Roll2d6DrdPlus;
 use DrdPlus\Codes\Skills\CombinedSkillCode;
 use Doctrine\ORM\Mapping as ORM;
+use DrdPlus\HuntingAndFishing\CatchProcessingQuality;
+use DrdPlus\Properties\Base\Knack;
 use DrdPlus\Skills\WithBonus;
 
 /**
@@ -28,6 +31,17 @@ class Cooking extends CombinedSkill implements WithBonus, \DrdPlus\HuntingAndFis
     public function getBonus(): int
     {
         return 2 * $this->getCurrentSkillRank()->getValue();
+    }
+
+    /**
+     * @link https://pph.drdplus.info/#hod_na_zpracovani_ulovku
+     * @param Knack $knack
+     * @param Roll2d6DrdPlus $roll2D6DrdPlus
+     * @return CatchProcessingQuality
+     */
+    public function createCatchProcessingQuality(Knack $knack, Roll2d6DrdPlus $roll2D6DrdPlus): CatchProcessingQuality
+    {
+        return new CatchProcessingQuality($knack, $this, $roll2D6DrdPlus);
     }
 
 }
--- DrdPlus/Tests/Skills/Combined/CookingTest.php
+++ DrdPlus/Tests/Skills/Combined/CookingTest.php
@@ -1,6 +1,9 @@
 <?php
 namespace DrdPlus\Tests\Skills\Combined;
 
+use Drd\DiceRolls\Templates\Rollers\Roller2d6DrdPlus;
+use DrdPlus\HuntingAndFishing\CatchProcessingQuality;
+use DrdPlus\Properties\Base\Knack;
 use DrdPlus\Skills\Combined\Cooking;
 
 class CookingTest extends WithBonusFromCombinedTest
@@ -14,5 +17,22 @@ class CookingTest extends WithBonusFromCombinedTest
             is_a(Cooking::class, \DrdPlus\HuntingAndFishing\Cooking::class, true),
             'Skill ' . Cooking::class . ' has to implement ' . \DrdPlus\HuntingAndFishing\Cooking::class
         );
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_create_catch_processing_quality_easier_by_this()
+    {
+        $cooking = new Cooking($this->createProfessionLevel());
+        $knack = Knack::getIt(5);
+        $roll2D6DrdPlus = Roller2d6DrdPlus::getIt()->roll();
+        $catchProcessingQuality = $cooking->createCatchProcessingQuality($knack, $roll2D6DrdPlus);
+        self::assertEquals(new CatchProcessingQuality($knack, $cooking, $roll2D6DrdPlus), $catchProcessingQuality);
+
+        $cooking->increaseSkillRank($this->createSkillPoint());
+        $higherCatchProcessingQuality = $cooking->createCatchProcessingQuality($knack, $roll2D6DrdPlus);
+        self::assertNotEquals($catchProcessingQuality, $higherCatchProcessingQuality);
+        self::assertEquals(new CatchProcessingQuality($knack, $cooking, $roll2D6DrdPlus), $higherCatchProcessingQuality);
     }
 }
