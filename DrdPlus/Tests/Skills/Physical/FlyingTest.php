7cedef4d427598bfcb163d7d7d51b830df0394b4
Jaroslav Týc <mail@jaroslavtyc.com>
2017-06-07 05:57:21 +0200

    Malus from flying tested

--- DrdPlus/Tests/Skills/Physical/FlyingTest.php
+++ DrdPlus/Tests/Skills/Physical/FlyingTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace DrdPlus\Tests\Skills\Physical;
 
+use DrdPlus\Skills\Physical\Flying;
+
 class FlyingTest extends WithBonusFromPhysicalTest
 {
     /**
@@ -10,6 +12,21 @@ class FlyingTest extends WithBonusFromPhysicalTest
     protected function getExpectedBonus(int $skillRankValue): int
     {
         return $skillRankValue * 2;
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_get_malus_from_flight()
+    {
+        $flying = new Flying($this->createProfessionLevel());
+        self::assertSame(-9, $flying->getMalusToFight());
+        $flying->increaseSkillRank($this->createSkillPoint());
+        self::assertSame(-6, $flying->getMalusToFight());
+        $flying->increaseSkillRank($this->createSkillPoint());
+        self::assertSame(-3, $flying->getMalusToFight());
+        $flying->increaseSkillRank($this->createSkillPoint());
+        self::assertSame(0, $flying->getMalusToFight());
     }
 
 }
