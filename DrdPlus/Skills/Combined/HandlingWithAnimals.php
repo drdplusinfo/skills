7e3ef48104d97754d6ef492e7c901ac1e2002c44
Jaroslav Týc <mail@jaroslavtyc.com>
2017-06-07 18:07:19 +0200

    Handling with animal can give bonus and if can soothe runaway animal

--- DrdPlus/Skills/Combined/HandlingWithAnimals.php
+++ DrdPlus/Skills/Combined/HandlingWithAnimals.php
@@ -3,11 +3,12 @@ namespace DrdPlus\Skills\Combined;
 
 use DrdPlus\Codes\Skills\CombinedSkillCode;
 use Doctrine\ORM\Mapping as ORM;
+use DrdPlus\Skills\WithBonus;
 
 /**
  * @ORM\Entity()
  */
-class HandlingWithAnimals extends CombinedSkill
+class HandlingWithAnimals extends CombinedSkill implements WithBonus
 {
     const HANDLING_WITH_ANIMALS = CombinedSkillCode::HANDLING_WITH_ANIMALS;
 
@@ -17,5 +18,21 @@ class HandlingWithAnimals extends CombinedSkill
     public function getName(): string
     {
         return self::HANDLING_WITH_ANIMALS;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBonus(): int
+    {
+        return $this->getCurrentSkillRank()->getValue() * 2;
+    }
+
+    /**
+     * @return bool
+     */
+    public function canSootheRunawayAnimal(): bool
+    {
+        return $this->getCurrentSkillRank()->getValue() >= 2;
     }
 }
